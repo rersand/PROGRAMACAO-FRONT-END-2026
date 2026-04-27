f2d6cd4eb6df556100d77f036f49a5a50276c4ce
André Ruan da Silva <andreruansilva16@gmail.com>
2026-04-27 14:30:09 -0300

    commit java script questão 4

--- site prova1/img/script.js
+++ site prova1/img/script.js
@@ -1,3 +1,33 @@
 function revelar(){
+function revelar() {
+    const imagemCopa = document.querySelector('.card-img-top');
+    imagemCopa.src = 'img/_vinicius_junior.png';
+
+    const tituloNome = document.getElementById('Nome');
+    const spanNome = tituloNome.querySelector('.placeholder'); 
+    const spanRank = document.getElementById('Rank');
+    const spanData = document.getElementById('Data_Nas');
+    const spanAltura = document.getElementById('Alutra'); 
+    const spanPosicao = document.getElementById('Posição '); 
+
+    spanNome.textContent = 'Vinícius José Paixão de Oliveira Júnior';
+    spanRank.textContent = '9,5';
+    spanData.textContent = '12/07/2000 (25 anos)';
+    spanAltura.textContent = '1,76 m';
+    spanPosicao.textContent = 'Ponta-esquerda / Atacante';
+
+    tituloNome.classList.remove('placeholder-glow');
     
+    const paragrafoText = document.querySelector('.card-text.placeholder-glow');
+    if (paragrafoText) {
+        paragrafoText.classList.remove('placeholder-glow');
+    }
+
+    const elementosParaAtualizar = [spanNome, spanData, spanAltura, spanPosicao];
+
+    elementosParaAtualizar.forEach(elemento => {
+        elemento.classList.remove('placeholder', 'col-4', 'col-6');
+        elemento.classList.add('card-text');
+    });
+}
 }
